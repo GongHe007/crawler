fa2443d6c3903ac781c5bb969eb2ea340d380081
gonghe <gonghe@creatingev.com>
2017-04-20 16:13:02 +0800

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,20 +2,13 @@
 lock "3.8.0"
 
 set :application, 'crawler'
-# 登陆服务器的用户名
-set :user, 'deployer'
-# 代码仓库的ssh地址
 set :repo_url, "git@github.com:GongHe007/crawler.git"
-# ruby版本，请查看rvm list中的对应信息
 set :rvm_ruby_version, '2.3.1'
-# 部署时输出的log级别，更详细的是:debug级别
 set :log_level, :info
-# 一些私密性的文件应该出现在这个队列中，因为他们是直接存放在服务器上的，capistrano部署的时候也会从这些地方读取对应的文件
 set :linked_files, %w{config/database.yml config/secrets.yml} # files we want symlinking to specific entries in shared.
-# 如果有一些公用的文档，可以让capistrano部署时为你生成
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
 
-# 在第一次部署时需要为puma的pid文件创建一个路径
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,9 @@
-role :app         ,  %w(lavps) # 在该服务器上执行app相关的代码
-role :web         ,  %w(lavps) # 在该服务器上执行web相关的代码，我们的app代码和web代码没有作区分
-role :db          ,  %w(lavps) # 在该服务器上执行migration相关的代码，在部署多台服务器的情况下，通常指定一台服务器，因为不能多次执行同一个migration
-role :production_cron,  %w{lavps} # 在该服务器上执行定时任务，将此定时任务的范围限定为sandbox_cron，使用whenever这个gem配置定时任务是会用到
+set :rails_env, 'production'
+set :deploy_to, "/var/www/#{fetch(:application)}"
+set :puma_threads, [8, 128]
+set :puma_workers, 8
+set :keep_releases, 64
 
-set :stage        ,   :production # 配置stage的名称为production
-set :rails_env    ,   :production # 配置rails_env的值为production
-set :branch       ,   :master # 配置代码仓库的分支名称
-set :deploy_to    ,   "/var/www/#{fetch(:application)}/#{fetch(:stage)}" # 配置项目在服务器上的部署位置
-set :puma_threads ,   [4, 16] # 配置puma的线程数量，这个值应该会影响并发的能力
-set :puma_workers ,   1 # 配置puma worker的数量，相当于运行几个项目的实例，跟CPU数量和性能有关
+role :app, %w{lavps}
+role :web, %w{lavps}
+role :db, %w{lavps}
