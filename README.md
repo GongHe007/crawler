d7fbfb2c10a04d5b8dae466a87185a93956718a4
gonghe <gonghe@creatingev.com>
2017-04-21 11:35:30 +0800

    fix code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 游戏新闻视频爬虫接口
-请求http://locahost:3000/api/videos?page=#{page}&per_page=#{per_page}
+请求http://128.1.67.159/api/videos/api/videos?page=#{page}&per_page=#{per_page}
 page是第几页，per_page是每页有多少条数据
 返回格式json:
 {
--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,6 +1,6 @@
 class Api::VideosController < ApiController
   def index
-    videos = Video.paginate(page: params[:page], per_page: params[:per_page])
+    videos = Video.order("id desc").paginate(page: params[:page], per_page: params[:per_page])
     render json: {videos: videos}
   end
 end
